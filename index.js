44846c25c39dd038b2066c6637351ea593a7e3de
Junior1253 <juliennepc25@gmail.com>
2025-08-31 22:00:44 +0000

    index.js

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const { Client, logger } = require('./lib/client')
 const { DATABASE, VERSION } = require('./config')
 const { stopInstance } = require('./lib/pm2')
+const fs = require('fs')
+const path = require('path')
+
+// Charger tous les plugins
+const pluginsDir = path.join(__dirname, 'plugins')
+fs.readdirSync(pluginsDir).forEach(file => {
+  if (file.endsWith('.js')) {
+    require(path.join(pluginsDir, file))
+    console.log("✅ Plugin chargé :", file) // Debug
+  }
+})
 
 const start = async () => {
-  logger.info(`levanter ${VERSION}`)
+  logger.info(`🛡️ SPIDER-NET SECURE-BOT v${VERSION}`)
   try {
     await DATABASE.authenticate({ retry: { max: 3 } })
   } catch (error) {
